dfb19ecf1efa6fe581a8bf7d8e80c53f71890884
serjsi <serykhs@gmail.com>
2021-10-12 20:19:16 +0300

    Colors? change activity

--- app/src/main/java/com/shpp/ssierykh/assignment2/MainActivity.kt
+++ app/src/main/java/com/shpp/ssierykh/assignment2/MainActivity.kt
@@ -2,10 +2,16 @@ package com.shpp.ssierykh.assignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        // Do something in response to button
+    }
+
 }

286951ec808a119d42495e65344b97ef32abce1c
serjsi <serykhs@gmail.com>
2021-10-15 14:35:36 +0300

    sign_in

--- app/src/main/java/com/shpp/ssierykh/assignment2/MainActivity.kt
+++ app/src/main/java/com/shpp/ssierykh/assignment2/MainActivity.kt
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
+
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 
 
--- app/src/main/java/com/shpp/ssierykh/assignment2/Sign_upActivity.kt
+++ app/src/main/java/com/shpp/ssierykh/assignment2/Sign_upActivity.kt
@@ -2,12 +2,27 @@ package com.shpp.ssierykh.assignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+
+
+
 
 class Sign_upActivity : AppCompatActivity() {
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up)
-    }
 
+
+    }
 
 }

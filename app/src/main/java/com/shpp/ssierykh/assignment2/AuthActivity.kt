77c5bb1199d264e23a83908be8276912da6b39cc
serjsi <serykhs@gmail.com>
2021-10-25 20:17:15 +0300

    Add TextInputLayout

--- app/src/main/java/com/shpp/ssierykh/assignment2/AuthActivity.kt
+++ app/src/main/java/com/shpp/ssierykh/assignment2/AuthActivity.kt
@@ -6,9 +6,12 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import android.content.Intent
-
-
-
+import android.net.wifi.p2p.WifiP2pManager
+import android.text.TextUtils
+import com.shpp.ssierykh.assignment2.Constants.TEST_EMAIL
+import com.shpp.ssierykh.assignment2.Constants.TEST_PASSWORD
+import android.text.TextWatcher;
+import java.lang.reflect.Array.newInstance
 
 
 class AuthActivity : AppCompatActivity() {
@@ -26,26 +29,38 @@ class AuthActivity : AppCompatActivity() {
 
         //Connect with the elements of our interface:
         username = (findViewById(R.id.editTextEnterEmail) as EditText?)!!
+
+
+
         password = findViewById(R.id.editTextTextPassword) as EditText?
+
+        
         sign_in = findViewById(R.id.sign_in) as Button?
         register = findViewById(R.id.buttonRegister) as Button?
 
         //Handle pressing the "SignIn" button:
-        sign_in?.setOnClickListener {
-            if (username?.text.toString() == "admin" && password?.text.toString() == "admin") {
+        val onClickListener = sign_in?.setOnClickListener {
+            if (username?.text.toString() == TEST_EMAIL && password?.text.toString() == TEST_PASSWORD) {
                 Toast.makeText(getApplicationContext(), "Enter!", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(getApplicationContext(), "Wrong password!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(getApplicationContext(), "Wrong password!", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
 
         //Switching to another screen
         register?.setOnClickListener{
-            val intent = Intent(this,MainActivity::class.java)
-            intent.putExtra("name",username?.text.toString())
-            intent.putExtra("myPhoto",R.drawable.my_photo)
-            startActivity(intent)
+            if (!TextUtils.isEmpty(username?.text)|| !TextUtils.isEmpty(password?.text) ) {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("name", username?.text.toString())
+                intent.putExtra("myPhoto", R.drawable.my_photo)
+                startActivity(intent)
+            }else {
+                Toast.makeText(getApplicationContext(), getString(R.string.empty_email_or_password), Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
+
 
 }
---
+++ app/src/main/java/com/shpp/ssierykh/assignment2/Constants.kt
@@ -0,0 +1,8 @@
+package com.shpp.ssierykh.assignment2
+
+object Constants {
+    const val TEST_EMAIL = "admin@gmail.com"
+    const val TEST_PASSWORD = "admin@gmail.com"
+    const val MIN_TEXT_LENGTH = 4
+
+}

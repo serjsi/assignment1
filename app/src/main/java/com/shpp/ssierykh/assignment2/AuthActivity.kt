be0aa533663d4e1e2b061592486e492f51c60e2c
serjsi <serykhs@gmail.com>
2021-10-29 15:45:21 +0300

    Corrected validate

--- app/src/main/java/com/shpp/ssierykh/assignment2/AuthActivity.kt
+++ app/src/main/java/com/shpp/ssierykh/assignment2/AuthActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.widget.Toast
 import android.content.Intent
 import android.text.Editable
-import android.text.TextUtils
 import android.text.TextWatcher
 import android.view.View
 import android.widget.TextView
@@ -14,10 +13,6 @@ import com.shpp.ssierykh.assignment2.Constants.TEST_EMAIL
 import com.shpp.ssierykh.assignment2.Constants.TEST_PASSWORD
 import androidx.databinding.DataBindingUtil
 import com.shpp.ssierykh.assignment2.Constants.MIN_LENGTH_PASSWORD
-import com.shpp.ssierykh.assignment2.Validators.isStringContainNumber
-import com.shpp.ssierykh.assignment2.Validators.isStringContainSpecialCharacter
-import com.shpp.ssierykh.assignment2.Validators.isStringLowerAndUpperCase
-import com.shpp.ssierykh.assignment2.Validators.isValidEmail
 import com.shpp.ssierykh.assignment2.databinding.ActivityAuthBinding
 
 
@@ -29,7 +24,6 @@ class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_auth)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_auth)
 
         setupListeners()
@@ -54,10 +48,7 @@ class AuthActivity : AppCompatActivity() {
 
         //Switching to another screen
         binding.buttonRegister.setOnClickListener {
-
-            if (isValidate() || !TextUtils.isEmpty(binding.editTextEnterEmail.text) ||
-                !TextUtils.isEmpty(binding.editTextTextPassword.text)
-            ) {
+            if ( validateEmail() && validatePassword()  ) {
                 val intent = Intent(this, MainActivity::class.java)
                 intent.putExtra("name", binding.editTextEnterEmail.text.toString())
                 intent.putExtra("myPhoto", R.drawable.lucile)
@@ -67,7 +58,7 @@ class AuthActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(
                     applicationContext,
-                    getString(R.string.empty_email_or_password),
+                    getString(R.string.wrong_email_or_password),
                     Toast.LENGTH_SHORT
                 )
                     .show()
@@ -78,51 +69,77 @@ class AuthActivity : AppCompatActivity() {
     }
 
 
-    private fun isValidate(): Boolean = validateEmail() && validatePassword()
-
     private fun setupListeners() {
         binding.editTextEnterEmail.addTextChangedListener(TextFieldValidation(binding.editTextEnterEmail))
         binding.editTextTextPassword.addTextChangedListener(TextFieldValidation(binding.editTextTextPassword))
     }
 
+    /**
+     * Сhecking validate password
+     */
     private fun validatePassword(): Boolean {
-
-        if (binding.editTextTextPassword.text.toString().trim().isEmpty()) {
-            binding.textInputLayoutPassword.error = "Required Field!"
-            binding.editTextTextPassword.requestFocus()
-            return false
-        } else if (binding.editTextTextPassword.text.toString().length < MIN_LENGTH_PASSWORD) {
-            binding.textInputLayoutPassword.error =
-                "Password can't be less than $MIN_LENGTH_PASSWORD"
-            binding.editTextTextPassword.requestFocus()
-            return false
-        } else if (!isStringContainNumber(binding.editTextTextPassword.text.toString())) {
-            binding.textInputLayoutPassword.error = "Required at least 1 digit"
-            binding.editTextTextPassword.requestFocus()
-            return false
-        } else if (!isStringLowerAndUpperCase(binding.editTextTextPassword.text.toString())) {
-            binding.textInputLayoutPassword.error =
-                "Password must contain upper and lower case letters"
-            binding.editTextTextPassword.requestFocus()
-            return false
-        } else if (!isStringContainSpecialCharacter(binding.editTextTextPassword.text.toString())) {
-            binding.textInputLayoutPassword.error = "1 special character required"
-            binding.editTextTextPassword.requestFocus()
-            return false
-        } else {
-            binding.textInputLayoutPassword.isErrorEnabled = false
+        val passwordChek = binding.editTextTextPassword.text.toString()
+        when {
+            //checking is empty
+            passwordChek.isEmpty() -> {
+                binding.textInputLayoutPassword.error = getString(R.string.message_cannot_be_empty)
+                binding.editTextTextPassword.requestFocus()
+                return false
+            }//checking is string min length
+            passwordChek.length < MIN_LENGTH_PASSWORD -> {
+                binding.textInputLayoutPassword.error =
+                    getString(R.string.password_cant_be_less_than)+ MIN_LENGTH_PASSWORD
+                binding.editTextTextPassword.requestFocus()
+                return false
+            }//checking is string contain any number
+            !passwordChek.contains(Regex("""\d""")) -> {
+                binding.textInputLayoutPassword.error = getString(R.string.required_at_least_1_digit)
+                binding.editTextTextPassword.requestFocus()
+                return false
+            }//checking is string contain lower case letters"
+            !passwordChek.contains(Regex("""[a-z]""")) -> {
+                binding.textInputLayoutPassword.error =
+                    getString(R.string.password_must_contain_lower_case_letters)
+                binding.editTextTextPassword.requestFocus()
+                return false
+            }//checking is string contain lower upper case letters"
+            !passwordChek.contains(Regex("""[A-Z]""")) -> {
+                binding.textInputLayoutPassword.error =
+                    getString(R.string.password_must_contain_upper_case_letters)
+                binding.editTextTextPassword.requestFocus()
+                return false
+            }//checking is string contain any special character
+            !passwordChek.contains(Regex("""[^a-zA-Z0-9]""")) -> {
+                binding.textInputLayoutPassword.error = getString(R.string._1_special_character_required)
+                binding.editTextTextPassword.requestFocus()
+                return false
+            }
+            else -> {
+                binding.textInputLayoutPassword.isErrorEnabled = false
+            }
         }
 
         return true
     }
 
+
+    /**
+     * Сhecking validate E-mail
+     */
     private fun validateEmail(): Boolean {
         if (binding.editTextEnterEmail.text.toString().trim().isEmpty()) {
-            binding.textInputLayoutEmail.error = "Required Field!"
+            binding.textInputLayoutEmail.error = getString(R.string.message_cannot_be_empty)
             binding.editTextEnterEmail.requestFocus()
             return false
-        } else if (!isValidEmail(binding.editTextEnterEmail.text.toString())) {
-            binding.textInputLayoutEmail.error = "Invalid Email!"
+            //} else if (!isValidEmail(binding.editTextEnterEmail.text.toString())) {
+        } else if (!binding.editTextEnterEmail.text.toString().contains(
+                Regex(
+                    "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
+                            "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+                )
+            )
+        ) {
+            binding.textInputLayoutEmail.error = getString(R.string.message_wromg_e_mail)
             binding.editTextEnterEmail.requestFocus()
             return false
         } else {
@@ -133,7 +150,7 @@ class AuthActivity : AppCompatActivity() {
 
 
     /**
-     * applying text watcher on each text field
+     * Applying text watcher on each text field
      */
     inner class TextFieldValidation(private val view: View) : TextWatcher {
         override fun afterTextChanged(s: Editable?) {}

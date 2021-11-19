f64781dfdf18f8208430dcffe8a920c383ef3e63
serjsi <serykhs@gmail.com>
2021-11-19 22:40:21 +0200

    Add PreferenceStorage.kt

--- app/src/main/java/com/shpp/ssierykh/assignment1/MainActivity.kt
+++ app/src/main/java/com/shpp/ssierykh/assignment1/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.shpp.ssierykh.assignment1
 
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Toast
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
-import androidx.lifecycle.asLiveData
 import com.shpp.ssierykh.assignment1.Constants.NAME_EXTRA
 import com.shpp.ssierykh.assignment1.Constants.NAME_SP
 import com.shpp.ssierykh.assignment1.Constants.PASSWORD_SP
@@ -16,25 +15,22 @@ import com.shpp.ssierykh.assignment1.Constants.PHOTO_EXTRA
 import com.shpp.ssierykh.assignment1.Constants.REMEMBER_SP
 import com.shpp.ssierykh.assignment1.Constants.TEST_EMAIL
 import com.shpp.ssierykh.assignment1.Constants.TEST_PASSWORD
+import com.shpp.ssierykh.assignment1.data.PreferenceStorage
 import com.shpp.ssierykh.assignment1.databinding.ActivityMainBinding
-import kotlinx.coroutines.DelicateCoroutinesApi
+
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var shPref: SharedPreferences
+    private  var shPref: PreferenceStorage = PreferenceStorage(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //Get reference to our userManager class
-
-
         setupListeners()
-
         loadAutologin()
         forTestMethod()
     }
@@ -42,7 +38,6 @@ class MainActivity : AppCompatActivity() {
     /**
      * Applying text watcher on each text field
      */
-    @DelicateCoroutinesApi
     private fun setupListeners() {
         binding.apply {
             etEnterEmail.doOnTextChanged { _, _, _, _ -> isValidateEmail() }
@@ -106,14 +101,14 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             val passwordChek = binding.etTextPassword.text.toString()
             Validators.validatePassword(passwordChek)
-            if (Validators.validatePassword(passwordChek) != 0) {
+            return if (Validators.validatePassword(passwordChek) != 0) {
                 tilPassword.error =
                     getString(Validators.validatePassword(passwordChek))
                 etTextPassword.requestFocus()
-                return false
+                false
             } else {
                 tilPassword.isErrorEnabled = false
-                return true
+                true
             }
         }
     }
@@ -122,24 +117,24 @@ class MainActivity : AppCompatActivity() {
      * Saving in SharedPreference autologin
      */
     private fun saveAutologin() {
-        shPref = getSharedPreferences("PrefAutologin", MODE_PRIVATE)
-        val ed: SharedPreferences.Editor = shPref.edit()
-        ed.putBoolean(REMEMBER_SP, binding.cbRemember.isChecked)
-        ed.putString(NAME_SP, binding.etEnterEmail.text.toString())
-        ed.putString(PASSWORD_SP, binding.etTextPassword.text.toString())
-        ed.commit()
+        binding.apply {
+            shPref.save(REMEMBER_SP, cbRemember.isChecked)
+            shPref.save(NAME_SP, etEnterEmail.text.toString())
+            shPref.save(PASSWORD_SP, etTextPassword.text.toString())
+        }
     }
 
     /**
      * Loading  SharedPreference autologin
      */
     private fun loadAutologin() {
-        shPref = getSharedPreferences("PrefAutologin", MODE_PRIVATE)
         val remember: Boolean = shPref.getBoolean(REMEMBER_SP, false)
         if (remember) {
-            binding.cbRemember.isChecked = true
-            binding.etEnterEmail.setText(shPref.getString(NAME_SP, ""))
-            binding.etTextPassword.setText(shPref.getString(PASSWORD_SP, ""))
+            binding.apply {
+                cbRemember.isChecked = true
+                etEnterEmail.setText(shPref.getString(NAME_SP, ""))
+                etTextPassword.setText(shPref.getString(PASSWORD_SP, ""))
+            }
         }
     }
 

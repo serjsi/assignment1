71c9bfcf3fbeb70fc928d8e6629857fde9aff3fe
serjsi <serykhs@gmail.com>
2022-09-11 22:52:59 +0300

    read test contacts for phone book

--- app/src/main/java/com/shpp/ssierykh/assignment1/activity_and_splash/MainActivity.kt
+++ app/src/main/java/com/shpp/ssierykh/assignment1/activity_and_splash/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.shpp.ssierykh.assignment1.activity_and_splash
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 
@@ -29,6 +37,7 @@ class MainActivity : AppCompatActivity(), Routing {
 
     // actions to be launched when activity is active
     private val actions = mutableListOf<() -> Unit>()
+    private lateinit var pLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -91,6 +100,60 @@ class MainActivity : AppCompatActivity(), Routing {
         actions.forEach { it() }
         actions.clear()
     }
+
+    private fun checkAddressReadPermission() {
+        when {
+            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) ==
+                    PackageManager.PERMISSION_GRANTED -> {
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) -> {
+                pLauncher.launch(Manifest.permission.READ_CONTACTS)
+            }
+            else -> {
+                pLauncher.launch(Manifest.permission.READ_CONTACTS)
+            }
+        }
+    }
+
+    private fun registerPermissionListener() {
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) {
+            if (it) {
+                Toast.makeText(this, "Read Contacts Allowed", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Read ContactsDenied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun downloadContactsPhone() {
+        Log.v("_______Contacts","start")
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) ==
+            PackageManager.PERMISSION_GRANTED
+        ) {
+
+            val cursor = this.contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null, null, null, null
+            )
+            cursor?.let {
+                while (it.moveToNext()) {
+                    val fullName =
+                        it.getString(it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                    val phone =
+                        it.getString(it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                    val newModel = Contact()
+                    newModel.name = fullName
+                    newModel.phone = phone
+                    //contacts.add(newModel)
+                    Log.v("_______Contacts","-$fullName--")
+                }
+            }
+            cursor?.close()
+        }
+    }
+
 
     /**
      * Avoiding [IllegalStateException] if navigation action has been called after restoring app from background.
